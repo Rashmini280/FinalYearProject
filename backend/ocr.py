dff7cb94393943f18f2c7a3779130181b7fe0d5a
Rashmini280 <rashmini.20221416@iit.ac.lk>
2026-01-28 14:04:48 +0530

    Initial structure of the backend

---
+++ backend/ocr.py
@@ -0,0 +1,7 @@
+# import PIL image
+# import pytesseract
+
+# def extract_text(image_path):
+#     img = Image.open(image_path).convert("RGB")
+#     text = pytesseract.image_to_string(img,lang="eng+sin")
+#     return text
