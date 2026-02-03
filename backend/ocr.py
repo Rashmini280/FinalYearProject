4911d9c3d10ca752de68264ca427275cd61318ab
Rashmini280 <rashmini.20221416@iit.ac.lk>
2026-02-03 23:45:08 +0530

    ocr

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,7 +1,8 @@
-# import PIL image
-# import pytesseract
 
-# def extract_text(image_path):
-#     img = Image.open(image_path).convert("RGB")
-#     text = pytesseract.image_to_string(img,lang="eng+sin")
-#     return text
+from PIL import Image
+import pytesseract
+
+def extract_text(image_path: str)-> str:
+    img = Image.open(image_path).convert("RGB")
+    text = pytesseract.image_to_string(img,lang="eng+sin")
+    return text.strip()  
