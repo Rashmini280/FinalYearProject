86f594d450967624cea68412b8ba7fe4f99e474e
Rashmini280 <rashmini.20221416@iit.ac.lk>
2026-03-26 21:15:30 +0530

    new start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ pip install --no-cache-dir -r requirements.txt
 
 # Start FastAPI with uvicorn
 # $PORT is the environment variable Railway provides
-uvicorn app:app --host 0.0.0.0 --port $PORT
+uvicorn app:app --host 0.0.0.0 --port ${PORT:-8000}
