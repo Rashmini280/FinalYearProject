63aca1af226444118bc7e99e2b9140bfa14d541f
Rashmini280 <137760714+Rashmini280@users.noreply.github.com>
2026-03-27 02:53:47 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-# Run app
-cd backend
-python -m uvicorn app:app --host 0.0.0.0 --port ${PORT:-8000}
